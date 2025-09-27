9bc1da49e8d05e391a9471f9769e6ba370dc6d01
abhishekjohri <abhishekjohri098@gmail.com>
2025-09-27 22:10:38 +0530

    Added Students POJO class

---
+++ src/main/java/com/orm/Students.java
@@ -0,0 +1,42 @@
+package com.orm;
+
+public class Students {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String tech;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+}
