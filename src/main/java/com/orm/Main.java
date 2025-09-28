584fbb6bdb7816736358bb179f75c54d1f74ef1b
abhishekjohri <abhishekjohri098@gmail.com>
2025-09-28 12:03:15 +0530

    CRUD Operations Hibernate

--- src/main/java/com/orm/Main.java
+++ src/main/java/com/orm/Main.java
@@ -1,9 +1,53 @@
 package com.orm;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+
+        Configuration config = new Configuration();
+        config.configure("hibernate.cfg.xml");
+        config.addAnnotatedClass(Students.class);
+
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        //create new
+        /*Students students = new Students();
+        students.setId(105);
+        students.setFirstName("Yogender");
+        students.setLastName("M");
+        students.setAge(25);
+        students.setTech("ML");
+        session.persist(students);*/
+
+        //fetch
+        //Students s = session.find(Students.class, 101);
+        //System.out.println(s);
+
+        //update/create
+        /*Students students1 =  new Students();
+        students1.setId(101);
+        students1.setFirstName("Abhi");
+        students1.setLastName("J");
+        students1.setAge(26);
+        students1.setTech("IS");
+        session.merge(students1);*/
+
+        //remove/delete
+        //Students s2 = session.find(Students.class, 103);
+        //session.remove(s2);
+
+        transaction.commit();
+
+        session.close();
+        factory.close();
+
 
     }
 }
--- src/main/java/com/orm/Students.java
+++ src/main/java/com/orm/Students.java
@@ -1,10 +1,28 @@
 package com.orm;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Students")
 public class Students {
 
+    @Id
+    @Column(name = "ID")
     private int id;
+
+    @Column(name = "FIRST_NAME")
     private String firstName;
+
+    @Column(name = "LAST_NAME")
     private String lastName;
+
+    @Column(name = "AGE")
+    private int age;
+
+    @Column(name = "TECH")
     private String tech;
 
     public int getId() {
@@ -39,4 +57,22 @@ public class Students {
         this.tech = tech;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", tech='" + tech + '\'' +
+                '}';
+    }
 }
